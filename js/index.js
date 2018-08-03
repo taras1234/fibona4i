9aa322240c100b1993cd5dfae360e13fe5fc9324
Taras <tarasbgluck@gmail.com>
2018-08-03 12:52:35 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
-const a = parseInt(prompt('Enter a number of fibonacci :'));
- 
-function fib(a) {
-  return a <= 1 ? a : fib(a - 1) + fib(a - 2);
-}
-const result = (fib(a));
-alert( fib(a) ); // 2
+//const a = parseInt(prompt('Enter a number of fibonacci :'));
+// 
+//function fib(a) {
+//  return a <= 1 ? a : fib(a - 1) + fib(a - 2);
+//}
+//const result = (fib(a));
+//alert( fib(a) ); // 2
 
+const n = parseInt(prompt('Enter n number of fibonacci :'))
+
+function fib(n) {
+  var a = 1,
+    b = 1;
+  for (var i = 3; i <= n; i++) {
+    var c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
+
+alert( fib(n) ); // 2
